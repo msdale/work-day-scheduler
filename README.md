daa54f2867ea66d24b585e36e2f1869f88c03db6
Mark Dale <msdaledad@gmail.com>
2021-11-15 18:24:16 -0500

    should be final

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # work-day-scheduler
 A Time Management (daily planner) Application
+
 ## Website
 https://msdale.github.io/work-day-scheduler/
+
+## URL image
+![Work-Day-Scheduler](./assets/images/work-day-scheduler.png)
---
+++ assets/js/script.js
@@ -0,0 +1,212 @@
+// global variables
+
+var hourParams = [
+  {
+    "hourId":
+      "hr1",
+    "hourTag":
+      "9AM",
+    "hourValue":
+      9
+  },
+  {
+    "hourId":
+      "hr2",
+    "hourTag":
+      "10AM",
+    "hourValue":
+      10
+  },
+  {
+    "hourId":
+      "hr3",
+    "hourTag":
+      "11AM",
+    "hourValue":
+      11
+  },
+  {
+    "hourId":
+      "hr4",
+    "hourTag":
+      "12PM",
+    "hourValue":
+      12
+  },
+  {
+    "hourId":
+      "hr5",
+    "hourTag":
+      "1PM",
+    "hourValue":
+      13
+  },
+  {
+    "hourId":
+      "hr6",
+    "hourTag":
+      "2PM",
+    "hourValue":
+      14
+  },
+  {
+    "hourId":
+      "hr7",
+    "hourTag":
+      "3PM",
+    "hourValue":
+      15
+  },
+  {
+    "hourId":
+      "hr8",
+    "hourTag":
+      "4PM",
+    "hourValue":
+      16
+  },
+  {
+    "hourId":
+      "hr9",
+    "hourTag":
+      "5PM",
+    "hourValue":
+      17
+  }
+];
+
+var eventsArray = [];
+
+
+// FUNCTIONS
+
+
+/** Save events in local storage */
+var saveEvents = function() {
+  localStorage.setItem("events", JSON.stringify(eventsArray));
+}
+
+/** Load events from local storage */
+var loadEvents = function() {
+  eventsArray = [];
+  eventsArray = JSON.parse(localStorage.getItem("events"));
+}
+
+/** Mark daily work hours for past (grey), present (red), and future (green) */
+var markPastPresentFuture = function (date) {
+  var curHour = date.getHours();
+  for (var i = 0; i < hourParams.length; i++) {
+    var hrVal = hourParams[i].hourValue;
+    var idVal = "#" + hourParams[i].hourId;
+    var hourId = hourParams[i].hourId;
+    if (hrVal < curHour) {
+      $(idVal).find("textarea").css("background-color", "#d3d3d3");
+      $(idVal).find("textarea").css("border-color", "black");
+      //$(idVal).find("textarea").prop("disabled", true);
+    } else if (hrVal > curHour) {
+      $(idVal).find("textarea").css("background-color", "#77dd77");
+      //$(idVal).find("textarea").prop("disabled", false);
+    } else if (hrVal === curHour) {
+      $(idVal).find("textarea").css("background-color", "#ff6961");
+      //$(idVal).find("textarea").prop("disabled", false);
+    } else {
+      throw "Data corruption...";
+    }
+
+    for (var j = 0; j < eventsArray.length; j++) {
+      if (eventsArray[j].id === hourId) {
+        $(idVal).find("textarea").text(eventsArray[j].text);
+      }
+    }
+  }
+};
+
+/** Print the day and month */
+var printDayMonthDDth = function (date) {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+
+  const days = [
+    "Sunday",
+    "Monday",
+    "TuesDay",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+
+  var month = months[date.getMonth()];
+  var monthDay = date.getUTCDate();
+  var day = days[date.getDay()];
+  var endingTxt = "th";
+  switch (day) {
+    case 1:
+    case 21:
+    case 31:
+      endingTxt = "st";
+      break;
+    case 2:
+    case 22:
+      endingTxt = "nd";
+      break;
+    case 3:
+    case 23:
+      endingTxt = "rd";
+      break;
+  }
+  var textDate = day + ", " + month + " " + monthDay + endingTxt;
+  $("#currentDay").text(textDate);
+};
+
+/** Catch the button click for saving event text and push to events array */
+$(":button").on("click", function (event) {
+  // get form values
+  var eventText = $(this)
+                .closest(".row")
+                .find("#text")
+                .find("textarea")
+                .val();
+
+  // find the hour id
+
+  var hourId = $(this)
+              .closest(".row")
+              .attr("id");
+
+  // insert the event text into the stored array
+
+  eventsArray.push({
+    text: eventText,
+    id: hourId
+  });
+
+  saveEvents();
+});
+
+/** Start up or refresh the Work Day Scheduler */
+var refresh = function () {
+  var date = new Date();
+  loadEvents();
+  printDayMonthDDth(date);
+  markPastPresentFuture(date);
+}
+
+refresh();
+
+/** refresh/update the Work Day Scheduler */
+setInterval(refresh, (60 * 1000));
+
+
